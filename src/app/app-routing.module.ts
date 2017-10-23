d8475b1c8ac488e51151e30b75d251947e56e675
tibytt <tibyttfj@gmail.com>
2017-10-23 00:28:31 -0300

    23/10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {RealhomeComponent} from "./realhome/realhome.component";
 import {HomeComponent} from "./home/home.component";
+import { FormularioComponent } from './formulario/formulario.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { PostsComponent } from './posts/posts.component';
 
 
-const routes: Routes = [{path: '', component:RealhomeComponent}];
+
+const routes: Routes = [{path: 'home', component:RealhomeComponent}, {path: 'contacto' , component:FormularioComponent },
+{path:"**", component: NotfoundComponent}, {path:"posts", component:PostsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
 import { LeftContentComponent } from './left-content/left-content.component';
 import { RightContentComponent } from './right-content/right-content.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,11 @@ import { RightContentComponent } from './right-content/right-content.component';
     FooterComponent,
     ContentComponent,
     LeftContentComponent,
-    RightContentComponent
+    RightContentComponent,
+    FormularioComponent,
+    NotfoundComponent,
+    NavbarComponent,
+    PostsComponent
   ],
   imports: [
     AppRoutingModule,
