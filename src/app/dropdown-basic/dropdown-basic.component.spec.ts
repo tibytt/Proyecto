71172f52ddccbf1b9e4f2cfa15622a175e13badc
tibytt <tibtt@gmail.com>
2017-10-23 22:01:20 -0300

    formulario

---
+++ src/app/dropdown-basic/dropdown-basic.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DropdownBasicComponent } from './dropdown-basic.component';
+
+describe('DropdownBasicComponent', () => {
+  let component: DropdownBasicComponent;
+  let fixture: ComponentFixture<DropdownBasicComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DropdownBasicComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DropdownBasicComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
